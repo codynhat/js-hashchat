64e642b48e99df768fba2209e6057d197d79329c
Cody Hatfield <cody.hatfield@me.com>
2022-06-25 14:47:19 -0700

    feat: Add authSession to ChatService

--- src/services/chat/chat.service.impl.ts
+++ src/services/chat/chat.service.impl.ts
@@ -6,6 +6,7 @@ import { AuthSession } from "../auth/auth.service";
 export class ChatServiceImpl extends ApiServiceImpl implements ChatService {
   userId?: UserId;
   private userToken?: string;
+  private authSession?: AuthSession;
 
   async authenticate(
     authSession: AuthSession
@@ -23,6 +24,7 @@ export class ChatServiceImpl extends ApiServiceImpl implements ChatService {
 
       this.userId = response.data["userId"];
       this.userToken = response.data["token"];
+      this.authSession = authSession;
       return this.success<UserId>(this.userId);
     } catch (e) {
       return this.error<UserId>(e);
